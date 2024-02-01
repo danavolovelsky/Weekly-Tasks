e0b3f4163e9fd33587fbedef69e5804c3e2fa4e1
Dana Volovelsky <danavolovelsky@MacBook-Pro-von-Dana.local>
2024-02-01 01:09:17 +0000

    upload

---
+++ Task 5/task5.js
@@ -0,0 +1,87 @@
+
+
+const studySpots = document.querySelector("#studySpots");
+const low = document.querySelector("#low"); // button for ascending order
+const high = document.querySelector("#high"); // button for descending order
+
+let ratingArray= [];
+
+async function getData () {
+    try {
+        const response = await fetch ('https://opensheet.elk.sh/1JVzI251KwjmqybTsCJ-avCFF005ErVaVDW7kwJzaAcU/Tabellenblatt1')
+        const data = await response.json()
+
+        ratingArray = data;
+        displayData(ratingArray)
+    } catch (err){
+        console.log(err);
+    }
+    
+}
+        function displayData(data){
+            studySpots.innerHTML = "";
+
+        data.forEach((row) => {
+            const container = document.createElement('div')
+            const name = document.createElement('h2')
+            const rating = document.createElement('h2')
+            const picture = document.createElement('img')
+            const prosButton = document.createElement('button')
+            const consButton = document.createElement('button')  
+            const pros = document.createElement('p')
+            const cons = document.createElement('p')  
+
+
+            container.append(name)
+            container.append(rating)
+            container.append(picture)
+            container.append(pros)
+            container.append(cons)
+            container.append(prosButton)
+            container.append(consButton)
+        
+            studySpots.append(container)
+            container.classList.add('container')
+            name.innerText = row.study_spots
+            rating.innerText = row.rating
+            picture.src = row.url
+            prosButton.innerText = "Pro"
+            consButton.innerText = "Con"
+            pros.innerText = row.Pros
+            cons.innerText = row.Cons
+
+            picture.setAttribute('alt', `Picture of the study spot ${row.study_spot}`);
+
+            pros.style.display = "none";
+            cons.style.display = "none";
+
+            prosButton.addEventListener('click', () => {
+                pros.style.display = 'block';
+                prosButton.style.display = "none";
+            });
+        
+
+
+            consButton.addEventListener('click', () => {
+                cons.style.display = 'block';
+                consButton.style.display = "none";
+
+            });
+        
+        } )
+    }
+
+low.addEventListener('click', () => {
+    ratingArray.sort((a, b) => parseFloat(a.rating) - parseFloat(b.rating)); // sort in ascending order
+    displayData(ratingArray);
+    window.scrollTo(0, 0);
+});
+
+high.addEventListener('click', () => {
+    ratingArray.sort((a, b) => parseFloat(b.rating) - parseFloat(a.rating)); // sort in descending order
+    displayData(ratingArray);
+    window.scrollTo(0, 0);
+
+});
+
+getData();
